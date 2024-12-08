e17e9f3d8b0a28dae447899a95dc0ef42a3fc0ae
Jacob Borup <jacobnb@outlook.dk>
2024-12-08 20:46:08 +0100

    Removed deprecated files

--- components/interface.tsx
+++
@@ -1,9 +0,0 @@
-export interface Schema {
-    queries: {
-      askBedrock: (args: { query: string; template: string }) => Promise<{
-        data?: { body?: string };
-        errors?: any;
-      }>;
-    };
-  }
-  
